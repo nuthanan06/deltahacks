f74727a98d9c73ebb12147ba60e8eb2e7a346a8d
Nuthanan Tharmarajah <n2tharma@uwaterloo.ca>
2026-01-10 11:17:52 -0500

    feat: webcam works

---
+++ backend/webcam.py
@@ -0,0 +1,22 @@
+from ultralytics import YOLO
+import cv2
+
+model = YOLO("yolov8n.pt")  # nano = fastest
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    results = model(frame)
+
+    annotated = results[0].plot()
+    cv2.imshow("YOLOv8", annotated)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
